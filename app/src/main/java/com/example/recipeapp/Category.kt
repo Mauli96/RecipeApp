c758b29113bdd5dc974b6a6feb1e56940a852ee4
Mauli Waghmore <mauliwaghmore9696@gmail.com>
2024-04-01 10:45:04 +0530

    Update Category.kt

--- app/src/main/java/com/example/recipeapp/Category.kt
+++ app/src/main/java/com/example/recipeapp/Category.kt
@@ -1,10 +1,14 @@
 package com.example.recipeapp
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
     val strCategoryDescription: String
-)
+) : Parcelable
 
 data class CategoriesResponse(val categories: List<Category>)
